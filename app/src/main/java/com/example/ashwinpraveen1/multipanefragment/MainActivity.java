5742fe9877109c8d23c55a08f286f951367cc608
Ashwin-Praveen <ashwinpraveen1@gmail.com>
2015-01-30 15:26:03 +0530

    Final commit

---
+++ app/src/main/java/com/example/ashwinpraveen1/multipanefragment/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.ashwinpraveen1.multipanefragment;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int screenOr = getResources().getConfiguration().orientation;
+
+        if(screenOr == Configuration.ORIENTATION_PORTRAIT) {
+            hideSide();
+        }
+    }
+
+    void hideSide() {
+        View sidePane = findViewById(R.id.fragment_side_panel);
+        if (sidePane.getVisibility() == View.VISIBLE ) {
+            sidePane.setVisibility(View.GONE);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/ashwinpraveen1/multipanefragment/MainPanel.java
@@ -0,0 +1,18 @@
+package com.example.ashwinpraveen1.multipanefragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainPanel extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_panel, container, false);
+        return view;
+
+    }
+}
